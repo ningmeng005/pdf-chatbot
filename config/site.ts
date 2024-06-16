eaad6478ac898c7b0a3b65fe495ff3229deb4a36
OliverLy670 <OliverLy670@gmail.com>
2024-06-16 18:24:01 +0800

    Modify the code

--- config/site.ts
+++ config/site.ts
@@ -32,6 +32,28 @@ const baseSiteConfig = {
         shortcut: "/favicon-16x16.png",
         apple: "/logo.png",
     },
+    headerLinks: [
+        { name: 'repo', href: "https://github.com/weijunext/clean-nextjs-starter", icon: BsGithub },
+        { name: 'twitter', href: "https://twitter.com/weijunext", icon: BsTwitterX },
+        { name: 'buyMeCoffee', href: "https://www.buymeacoffee.com/weijunext", icon: SiBuymeacoffee }
+    ],
+    footerLinks: [
+        { name: 'email', href: "mailto:weijunext@gmail.com", icon: MdEmail },
+        { name: 'twitter', href: "https://twitter.com/weijunext", icon: BsTwitterX },
+        { name: 'github', href: "https://github.com/weijunext/", icon: BsGithub },
+        { name: 'buyMeCoffee', href: "https://www.buymeacoffee.com/weijunext", icon: SiBuymeacoffee },
+        { name: 'juejin', href: "https://juejin.cn/user/26044008768029", icon: SiJuejin },
+        { name: 'weChat', href: "https://weijunext.com/make-a-friend", icon: BsWechat }
+    ],
+    footerProducts: [
+        { url: 'https://weijunext.com/', name: 'J实验室' },
+        { url: 'https://smartexcel.cc/', name: 'Smart Excel' },
+        { url: 'https://landingpage.weijunext.com/', name: 'Landing Page Boilerplate' },
+        { url: 'https://nextjs.weijunext.com/', name: 'Next.js Practice' },
+        { url: 'https://starter.weijunext.com/', name: 'Next.js Starter' },
+        { url: 'https://githubbio.com', name: 'Github Bio Generator' },
+        { url: 'https://github.com/weijunext/indie-hacker-tools', name: 'Indie Hacker Tools' },
+    ]
 }
 
 export const siteConfig: SiteConfig = {
@@ -51,5 +73,4 @@ export const siteConfig: SiteConfig = {
         images: [`${baseSiteConfig.url}/og.png`],
         creator: baseSiteConfig.creator,
     },
-    // 不需要再提供 headerLinks, footerLinks, footerProducts
 }
